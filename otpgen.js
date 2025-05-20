d8e3c49ff615fcc82bae4232400b4d979307e67a
tarun <tarunjangra1903@gmail.com>
2025-05-20 19:40:14 +0530

    otpgenerator

---
+++ otpgen.js
@@ -0,0 +1,9 @@
+// otp generator 
+const min = 1000
+const max = 9999
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
+// let otp = (Math.random()*(9999-1000+1)+1000)
+// console.log(Math.round(otp))
