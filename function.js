e683caa3d7ea17fbadd40900fa8d9bd9132e080a
tarun <tarunjangra1903@gmail.com>
2025-05-22 01:11:59 +0530

    function practice

---
+++ function.js
@@ -0,0 +1,69 @@
+// function addtwoNumber(num1, num2){
+//     let result = num1+num2
+//     return result
+// }
+// let result = addtwoNumber(2,4)
+// console.log(result);
+
+// function Table(num){
+//     let i, multi=1;
+
+//     for(i=1;i<=10;i++){
+//         multi = i * num
+//         console.log(`${num} * ${i} = ${multi}`)
+//     }
+// }
+// Table(2)
+
+
+// function factorial(num){
+//      let fact = 1 ,i 
+//      for(i=num;i>=1;i--)
+//         fact = fact*i   
+//     return fact
+// }
+// let fact = factorial(3)
+// console.log(fact)
+
+
+//object through function 
+
+// let object = {
+//     name:"tarun",
+//     age:19,
+//     city:"Panipat"
+// }
+// function handleObj(Obj){
+//     return `hi my name is ${object.name} i'm ${object.age} old and i live in ${object.city} ` 
+// }
+// console.log(handleObj())
+
+
+// direct use of object while calling the function through passing object as argument
+// function handleObj(anyobject){
+//     return `hi my name is ${anyobject.name} i'm ${anyobject.age} old and i live in ${anyobject.city} ` 
+// }
+// console.log(handleObj({
+//     name:"tarun",
+//     age:19,
+//     city:"Panipat"
+
+// }))
+
+
+
+
+//Array through function
+
+// let arr = [100,200,22,322]
+// function Arrayaccess(arr){
+//     return arr[1]
+// }
+// console.log(Arrayaccess(arr));
+
+// direct passing array as an argument
+
+// function Arrayaccess(arr){
+//     return arr[3]
+// }
+// console.log(Arrayaccess([100,200,22,322]))
