3bf3ba582696078ea9095a3bf5366dc002fcc67f
tarun <tarunjangra1903@gmail.com>
2025-05-22 10:25:18 +0530

    hoisting

--- scope.js
+++ scope.js
@@ -1,10 +1,38 @@
 // local scope
-// like if else block for loop after that we use {} which makes a block inside of that scope we declare something is known as local scope 
-if(true){
-    // if i use something here is start from { and end on the }
-    let a = 30; // a is a local variable and i cant access outside of the if block because of local scope
-    console.log("INNER:", a); 
-}
-// global scope
-let a =300 
-console.log(a);
+// // like if else block for loop after that we use {} which makes a block inside of that scope we declare something is known as local scope 
+// if(true){
+//     // if i use something here is start from { and end on the }
+//     let a = 30; // a is a local variable and i cant access outside of the if block because of local scope
+//     console.log("INNER:", a); 
+// }
+// // global scope
+// let a =300 
+// console.log(a);
+
+
+// function incrementByOne(num){
+//     return num + 1 
+// }
+// console.log(incrementByOne(5));
+
+
+// can define later after calling a function 
+// console.log(incrementByOne(5));
+// function incrementByOne(num){
+//     return num + 1 
+// }
+
+
+// variable function or an expression
+// let addone = function(num){
+//     return num+1
+// }
+// console.log(addone(2));
+
+
+
+//  if i try to access a variable function before defining the function it may cause an error
+// console.log(addone(2));
+// let addone = function(num){
+//     return num+1
+// }
