ca82191ceb697ea13ebf99972d1d90aa8fef45d8
tarun <tarunjangra1903@gmail.com>
2025-05-22 19:49:40 +0530

    calculator with function

--- function.js
+++ function.js
@@ -67,3 +67,49 @@
 //     return arr[3]
 // }
 // console.log(Arrayaccess([100,200,22,322]))
+
+
+// **************************************** calculator with function*********************************************
+
+
+
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
+
+// console.log(calculateSum(2, 5));
+// console.log(calculateSum(10, 10));
+// console.log(calculateSum(5, 5));
+
+function calculateDifference(num1, num2) {
+  return num1 - num2;
+}
+
+// console.log(calculateDifference(22, 5));
+// console.log(calculateDifference(12, 1));
+// console.log(calculateDifference(17, 9));
+
+function calculateProduct(num1, num2) {
+  return num1 * num2;
+}
+
+// console.log(calculateProduct(13, 5));
+
+
+function calculateQuotient(num1, num2) {
+  return num2 === 0 ? "Error: Division by zero" : num1 / num2;
+}
+
+// console.log(calculateQuotient(7, 11));
+// console.log(calculateQuotient(3, 0));
+
+function calculateSquare(num) {
+  return num ** 2;
+}
+
+// console.log(calculateSquare(2));
+// console.log(calculateSquare(9));
+
+function calculateSquareRoot(num) {
+  return Math.sqrt(num);
+}
